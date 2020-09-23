abe1e1088663b956cda20eafcf566b3ede64ab64
Ashish Sharma <ashish.shm09@gmail.com>
2020-09-24 04:17:50 +0530

    Public/Private methods

---
+++ codeacademy/banking_on_ruby.rb
@@ -0,0 +1,40 @@
+class Account
+    attr_reader :name, :balance
+    def initialize(name, balance=10000)
+      @name = name
+      @balance = balance
+    end
+
+    private
+    
+    def pin
+      @pin = 1234
+    end
+    
+    def pin_error
+      "Access denied: incorrect PIN."
+    end
+
+    public
+    
+    def display_balance(pin_number)
+      puts pin_number == pin ? "Balance: $#{@balance}." : pin_error
+    end
+    
+    def withdraw(pin_number, amount)
+      if pin_number == pin
+        @balance -= amount
+        puts "Withdrew #{amount}. New balance: $#{@balance}."
+      else
+        puts pin_error
+      end
+    end
+    
+    
+  end
+  
+  my_account = Account.new("Ashish", 1_000_000)
+  my_account.withdraw(1123, 500_000)
+  my_account.display_balance(1234)
+  my_account.withdraw(1234, 500_000)
+  my_account.display_balance(1234)
